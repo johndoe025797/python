d9a9d9b6ca359451761bb1621b617e410bcfa09d
johndoe025797 <vova.beluga@gmail.com>
2025-01-09 23:32:34 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,8 @@
+from fake_math import divide as fake_divide
+from true_math import divide as true_divide
+
+result1 = fake_divide(10, 0)
+result2 = true_divide(7, 0)
+    
+print(result1)
+print(result2)
